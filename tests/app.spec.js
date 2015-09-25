8768f22f5eac123afa1a4d48bcca822a3d90b441
Antony <anton.plotnikov@gmail.com>
2015-09-25 21:43:49 +0600

    remove unused lib

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,16 +1,9 @@
 describe('App', function () {
     'use strict';
 
-    var assert = require('assert'),
-        sinon = require('sinon'),
-        bro = require('jsdom-test-browser'),
-        React = require('react'),
-        TestUtils = require('react/addons').addons.TestUtils;
-
-    before(function (done) { bro.jQueryify(done); });
+    var assert = require('assert');
 
     describe('default HTML', function () {
-
         it('...', function () {
             assert(2 === 2);
         });
